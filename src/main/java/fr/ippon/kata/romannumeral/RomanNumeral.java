8567e749207bef83a0754c489d62a09f8a936288
aissabnhd <aissa.bnhd@gmail.com>
2023-10-02 15:29:24 +0200

    (Refactor): Rename file + implement skeletons

---
+++ src/main/java/fr/ippon/kata/romannumeral/RomanNumeral.java
@@ -0,0 +1,8 @@
+package fr.ippon.kata.romannumeral;
+
+public class RomanNumeral {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
---
+++ src/test/java/fr/ippon/kata/romannumeral/RomanNumeralTest.java
@@ -0,0 +1,12 @@
+package fr.ippon.kata.romannumeral;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RomanNumeralTest {
+
+    @Test
+    public void test(){
+        Assertions.assertTrue(true);
+    }
+}
