4c5f1e3f6c7c188759d73def05775736b2ff9e49
MathisHadi <mathis.hadinger@gmail.com>
2024-10-08 11:57:21 +0200

    first part

--- src/main/java/fr/ippon/kata/romannumeral/RomanNumeral.java
+++ src/main/java/fr/ippon/kata/romannumeral/RomanNumeral.java
@@ -1,8 +1,24 @@
 package fr.ippon.kata.romannumeral;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class RomanNumeral {
 
     public static void main(String[] args) {
         System.out.println("Test");
     }
+
+    public static String RomainCalc(int num){
+        String result = "I";
+        if(num <= 3){
+            result = StringUtils.repeat(result, num);
+        }
+
+
+
+        return result;
+
+    }
+
+
 }
--- src/test/java/fr/ippon/kata/romannumeral/RomanNumeralTest.java
+++ src/test/java/fr/ippon/kata/romannumeral/RomanNumeralTest.java
@@ -3,10 +3,49 @@ package fr.ippon.kata.romannumeral;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static fr.ippon.kata.romannumeral.RomanNumeral.RomainCalc;
+
 public class RomanNumeralTest {
 
     @Test
     public void test(){
         Assertions.assertTrue(true);
     }
+
+    @Test
+    public void Test_UN() {
+
+        final int int_un = 1;
+
+        final String result = RomainCalc(int_un);
+
+        Assertions.assertEquals("I", result);
+    }
+
+
+    @Test
+    public void Test_table() {
+
+        final int MAX = 3;
+
+        List<String> table_result = new ArrayList<>();
+
+        for(int i=1; i<=MAX; i++){
+            final String result = RomainCalc(i);
+            table_result.add(result);
+        }
+
+
+        Assertions.assertEquals("I", table_result.get(0));
+        Assertions.assertEquals("II", table_result.get(1));
+       Assertions.assertEquals("III", table_result.get(2));
+
+
+
+    }
+
+
 }
