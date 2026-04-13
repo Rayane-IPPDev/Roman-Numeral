fa82c1e45b4aa802dbfe9e645468d67f6b5388f4
unknown <bouaziz.nour@gmail.com>
2026-04-13 15:05:02 +0200

    commit for 1 to 3

--- src/main/java/fr/ippon/kata/romannumeral/RomanNumeral.java
+++ src/main/java/fr/ippon/kata/romannumeral/RomanNumeral.java
@@ -5,4 +5,15 @@ public class RomanNumeral {
     public static void main(String[] args) {
         System.out.println("Test");
     }
+
+    public static String covert(int i) {
+        if (i<4) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 1; j < i +1; j++) {
+                sb.append("I");
+            }
+            return sb.toString();
+        }
+        return null;
+    }
 }
--- src/test/java/fr/ippon/kata/romannumeral/RomanNumeralTest.java
+++ src/test/java/fr/ippon/kata/romannumeral/RomanNumeralTest.java
@@ -3,10 +3,22 @@ package fr.ippon.kata.romannumeral;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanNumeralTest {
 
     @Test
     public void test(){
         Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void test_given_1_then_rerurn_I (){
+        assertEquals("I", RomanNumeral.covert(1));
+    }
+
+    @Test
+    public void test_given_3_then_rerurn_III (){
+        assertEquals("III", RomanNumeral.covert(3));
     }
 }
